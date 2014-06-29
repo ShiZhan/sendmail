39d88330d77f28dba9a1895e21a462e254c94c81
ShiZhan <g.shizhan.g@gmail.com>
2014-06-28 22:20:13 -0500

    attachment

--- src/main/scala/SendMail.scala
+++ src/main/scala/SendMail.scala
@@ -1,26 +1,32 @@
 object SendMail {
   import kernel.Parser._
+
   val senderSample = getClass.getResourceAsStream("sample-sender-gmail.conf")
-  val usage = """usage: sendmail [-hftcbs] [message]
+
+  val usage = """Usage: sendmail [-hftcbs] [message]
+
  -h   print this message
- -f   [from],    sender information (typical config file)
- -t   [to],      receiver addresses
- -c   [cc],      cc addresses
- -b   [bcc],     bcc addresses
- -s   [subject], mail subject
-      [message], mail content (read from stdin if empty)
+ -f   [from],       sender information (typical config file)
+ -t   [to],         receiver addresses
+ -c   [cc],         cc addresses
+ -b   [bcc],        bcc addresses
+ -a   [attachment], add a local file as attachment
+ -s   [subject],    mail subject
+      [message],    mail content (read from stdin if empty)
 
 Use ',' to separate multiple addresses, without spaces.
 
 Sender config sample:
 
 """ + io.Source.fromInputStream(senderSample).mkString
+
   val incorrectArgs = "Incorrect parameters, see help (sendmail -h)."
 
   def main(args: Array[String]) = args.toList match {
     case "-h" :: Nil => println(usage)
     case optList => parse(optList).options match {
-      case Some((from, to, cc, bc, subject, message)) => from.send(to, cc, bc, subject, message)
+      case Some((from, to, cc, bc, subject, message, attachment)) =>
+        from.send(to, cc, bc, subject, message, attachedFile = attachment)
       case _ => println(incorrectArgs)
     }
   }
--- src/main/scala/kernel/Parser.scala
+++ src/main/scala/kernel/Parser.scala
@@ -1,10 +1,12 @@
 package kernel
 
 object Parser extends helper.Logging {
+  import java.io.File
+
   type Options = Map[Symbol, Any]
 
   def loadSender(from: String) = try {
-    val configFile = new java.io.File(from)
+    val configFile = new File(from)
     val config = com.typesafe.config.ConfigFactory.parseFile(configFile)
     val hostName = config.getString("email.hostname")
     val smtpPort = config.getInt("email.port")
@@ -38,6 +40,10 @@ object Parser extends helper.Logging {
       case "-b" :: bc :: more =>
         val whispers = bc.split(',')
         if (whispers.forall(isValidEmail)) parse(more) ++ Map('bc -> whispers) else parse(more)
+      case "-a" :: attachment :: more =>
+        val attachedFile = new File(attachment)
+        if (attachedFile.exists) parse(more) ++ Map('attachment -> attachedFile)
+        else parse(more)
       case "-s" :: subject :: more => parse(more) ++ Map('subject -> subject)
       case message :: more => parse(more) ++ Map('message -> message)
     }
@@ -46,10 +52,12 @@ object Parser extends helper.Logging {
     val to = o.get('to) match { case Some(t: Array[String]) => t; case _ => Array[String]() }
     val cc = o.get('cc) match { case Some(c: Array[String]) => c; case _ => Array[String]() }
     val bc = o.get('bc) match { case Some(b: Array[String]) => b; case _ => Array[String]() }
+    val attachment = o.get('attachment) match { case Some(f: File) => Some(f); case _ => None }
     val subject = o.get('subject) match { case Some(s: String) => s; case _ => "Untitled" }
     val message = o.get('message) match { case Some(m: String) => Some(m); case _ => None }
     def options = o.get('from) match {
-      case Some(from: Sender) if o.contains('to) => Some(from, to, cc, bc, subject, message)
+      case Some(from: Sender) if o.contains('to) =>
+        Some(from, to, cc, bc, subject, message, attachment)
       case _ => None
     }
   }
